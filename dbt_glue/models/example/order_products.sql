a834abbba0bc325e8832213a45445980aee72586
chypwc <chienyeh5@gmail.com>
2025-06-19 02:01:45 +1000

    edit dbt project

---
+++ dbt_glue/models/example/order_products.sql
@@ -0,0 +1,24 @@
+{{ config(
+    materialized='incremental',
+    file_format='parquet',
+    incremental_strategy='insert_overwrite',
+    partition_by=['eval_set']
+) }}
+
+SELECT
+    order_id,
+    product_id,
+    add_to_cart_order,
+    reordered,
+    'prior' AS eval_set
+FROM {{ ref('order_products__prior') }}
+
+UNION ALL
+
+SELECT
+    order_id,
+    product_id,
+    add_to_cart_order,
+    reordered,
+    'train' AS eval_set
+FROM {{ ref('order_products__train') }}
