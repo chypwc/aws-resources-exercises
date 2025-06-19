fb62df69c05002a2c75ea52e5f9aaffc44c3e414
chypwc <chienyeh5@gmail.com>
2025-06-20 00:58:38 +1000

    update dbt models

--- dbt_glue/models/silver/prd_features.sql
+++ dbt_glue/models/silver/prd_features.sql
@@ -21,7 +21,7 @@ SELECT
     COUNT(*) AS total_purchases,
     SUM(CASE WHEN reordered = 1 THEN 1 ELSE 0 END) AS total_reorder,
     SUM(CASE WHEN product_seq_time = 1 THEN 1 ELSE 0 END) AS first_time_purchases,
-    SUM(CASE WHEN product_seq_time = 2 THEN 1 ELSE 0 END) AS second_time_purchases,
+    SUM(CASE WHEN product_seq_time = 2 THEN 1 ELSE 0 END) AS second_time_purchases
 FROM product_seqquence
 GROUP BY product_id
 ORDER BY product_id
--- dbt_glue/models/silver/user_features_1.sql
+++ dbt_glue/models/silver/user_features_1.sql
@@ -6,7 +6,7 @@
 SELECT
     user_id,
     MAX(order_number) AS max_order_number,
-    SUM(days_since_prior) AS total_days_since_prior,
-    AVG(days_since_prior) AS avg_days_since_prior
+    SUM(days_since_prior_order) AS total_days_since_prior,
+    AVG(days_since_prior_order) AS avg_days_since_prior
 FROM {{ source('sources', 'orders') }}
 GROUP BY user_id
